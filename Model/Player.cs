68c0438cf4228c46374e3a4927380e03b07afd2b
Szabó Bence <szabence029@gmail.com>
2024-08-16 22:23:32 +0200

    Player class

---
+++ Model/Player.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Gmtk2024.Model
+{
+    public class Player
+    {
+        private String _name;
+        private int _lives;
+
+        public Player(String name, int lives)
+        {
+            _name = name;
+            _lives = lives;
+        }
+
+        public void DecreaseLives()
+        {
+            _lives--;
+        }
+
+        public int GetLives()
+        {
+            return _lives;
+        }
+    }
+}
+
