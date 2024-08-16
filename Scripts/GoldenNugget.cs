bacfc43a43640d88a7bd24e0078aa26c85e23c69
hrajmund <hubairajmund0507@gmail.com>
2024-08-16 23:55:25 +0200

    Last push for today gn

--- Scripts/GoldenNugget.cs
+++ Scripts/GoldenNugget.cs
@@ -1,22 +1,44 @@
 using Godot;
 using System;
+using Gmtk2024.Model;
 
-public class GoldenNugget : Node2D
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-    // Called when the node enters the scene tree for the first time.
-    private Sprite sprite;
-    public override void _Ready()
+namespace Gmtk2024.Scripts{
+    public class GoldenNugget : Node2D
     {
-        sprite = GetNode<Sprite>("Sprite");
-        ApplyScale(new Vector2(5,0));
+        private Sprite sprite;
+        private Polygon2D polygon;
+        public override void _Ready()
+        {
+            sprite = GetNode<Sprite>("Sprite");
+            polygon = GetNode<Polygon2D>("Polygon2D");
+        }
+
+        public void setPolygon(PolygonType polygonType){
+            try {
+                switch(polygonType){
+                    case PolygonType.Circle : 
+                        //Draw Circle
+                        break;
+                    case PolygonType.Triangle : 
+                        //Draw Triangle
+                        break;
+                    case PolygonType.Square :
+                        //Draw Square
+                        break;
+                    default: throw new NullReferenceException(); 
+                }
+            }catch (Exception e){
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
