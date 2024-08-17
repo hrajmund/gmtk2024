577dbe149f235b829c6bba61f06f2a57017947df
tothzalan <toth.zalan03@gmail.com>
2024-08-17 13:07:35 +0200

    Scripts/Problem: working generate_problem()

--- Scripts/Problem.gd
+++ Scripts/Problem.gd
@@ -6,11 +6,21 @@ var operation_script = preload("res://Model/OperationHelper.cs")
 var operation_helper = operation_script.new()
 
 
-func generate_problem():
-	print(operation_helper)
-	var test = operation_helper.GetAddition()
-	if test == operation_helper.GetAddition():
-		print("working")
+func generate_problem(n: int) -> Array:
+	var problems = []
+	
+	var possibilities = [
+		operation_helper.GetAddition(),
+		operation_helper.GetSubtraction(),
+		operation_helper.GetMultiplication(),
+		operation_helper.GetDivination(),
+	]
+	
+	for i in range(n):
+		var choice = possibilities[randi() % possibilities.size()]
+		problems.append(choice)
+	
+	return problems
 
 func _ready():
-	generate_problem()
+	print(generate_problem(number_of_steps))
