e131b18d7b4b510a3793e1ec6aee9100f9eb319e
Kaiser László <kaiser.lacko@gmail.com>
2024-08-17 11:42:00 +0200

    Card, hand changes

--- Scripts/Hand.cs
+++ Scripts/Hand.cs
@@ -2,23 +2,54 @@ using Godot;
 using System;
 using System.Collections.Generic;
 using Gmtk2024.Model;
+using Gmtk2024.Scripts;
+using Reference = Gmtk2024.Reference;
 
 public class Hand : Node2D
 {
-	public PackedScene Card;
+	[Export]
+	public PackedScene CardScene;
 
+	private List<List<Effect>> _effects;
 
-	public void AddCards(List<List<Effect>> effects)
+	public override void _Ready()
 	{
-		
+		List<List<Effect>> eff = new List<List<Effect>>();
+
+		for (int i = 0; i < 10; i++)
+		{
+			eff.Add(new List<Effect>());
+		}
+
+		SetCards(eff);
+	}
+
+	public void SetCards(List<List<Effect>> effects)
+	{
+		ClearCards();
+		_effects = effects;
+
+		int offset = 150;
+		float startingPos = 0 - (effects.Count / 2) * offset;
+
+		int i = 0;
+		foreach (var effect in _effects)
+		{
+			Card inst = CardScene.Instance() as Card;
+			AddChild(inst);
+			
+			inst.Position = Position;
+			inst.LookAt(new Vector2(GlobalPosition.x + 300, GlobalPosition.y - 200));
+			inst.Setup(i, new Vector2(startingPos, Position.y), (int)inst.RotationDegrees);
+
+			i++;
+			startingPos += offset;
+		}
 	}
 
 	public void ClearCards()
 	{
-		for (int i = 0; i < GetChildCount(); i++)
-		{
-			GetChild(i).QueueFree();
-		}
+		GetTree().CallGroup(Reference.CardGroup, "TriggerRemove");
 	}
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
