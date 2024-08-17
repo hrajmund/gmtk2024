a33b47c78f9d57d2f70c181ba075d7c4cb211670
tothzalan <toth.zalan03@gmail.com>
2024-08-17 15:42:03 +0200

    UI rot

--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -1,7 +1,6 @@
 extends Control
 
 export(int) var health = 3
-
 export(int) var level = 1
 
 
@@ -12,11 +11,11 @@ var level_label
 
 func _ready():
 	max_width = get_parent().get_size().x
-	var panel_container = $PanelContainer
+	var panel_container = $MarginContainer
 	panel_container.size_flags_horizontal = Control.SIZE_EXPAND + Control.SIZE_SHRINK_END
 	
-	health_label = $PanelContainer/GridContainer/HealthLabel
-	level_label = $PanelContainer/GridContainer/LevelLabel
+	health_label = $MarginContainer/GridContainer/HealthLabel
+	level_label = $MarginContainer/GridContainer/LevelLabel
 
 
 
