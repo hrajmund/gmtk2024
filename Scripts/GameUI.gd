75377e3be73de329270a1643e3b7621bbde616bb
tothzalan <toth.zalan03@gmail.com>
2024-08-17 17:41:25 +0200

    UI rot

--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -6,7 +6,6 @@ export(int) var level = 1
 
 var max_width
 
-var health_label
 var health_sprite
 var level_label
 
@@ -26,13 +25,12 @@ func _ready():
 	var panel_container = $MarginContainer
 	panel_container.size_flags_horizontal = Control.SIZE_EXPAND + Control.SIZE_SHRINK_END
 	
-	health_label = $MarginContainer/GridContainer/HealthLabel
+
 	health_sprite = $MarginContainer/GridContainer/HealthSprite
 	level_label = $MarginContainer/GridContainer/LevelLabel
 
 
 func _process(delta):
-	health_label.text = "Health: " + str(health)
 	level_label.text = "Level: " + str(level)
 	
 	if health > 3:
