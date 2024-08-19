bd0b973f733ef796001813483c06e88e58e642b3
tothzalan <toth.zalan03@gmail.com>
2024-08-19 18:57:10 +0200

    Scenes/Music: is_playing variable

--- Scenes/Music.gd
+++ Scenes/Music.gd
@@ -1,12 +1,16 @@
 extends Node
 
 var music
+var is_playing = false
 
 func _ready():
 	music = $MusicFile
 
 func playMusic():
-	music.play()
+	if not is_playing:
+		music.play()
+		is_playing = true
 	
 func stopMusic():
 	music.stop()
+	is_playing = false
